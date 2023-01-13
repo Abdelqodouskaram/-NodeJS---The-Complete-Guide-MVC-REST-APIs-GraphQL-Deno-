3ed1cffa2c1ea21b0d986029c933721139808aa9
AbdElQodous <a.karam@intcore.net>
2023-01-13 16:24:25 +0200

    node mvc

--- app.js
+++ app.js
@@ -25,4 +25,6 @@ app.use(shopRoutes);
 
 app.use(errorsController.get404);
 
-app.listen(8080);
+app.listen(8080, () => {
+    console.log("Server is running on port 8080");
+});
--- models/product.js
+++ models/product.js
@@ -1,15 +1,32 @@
-const products = [];
+const path = require("path");
+const fs = require("fs");
+const p = path.join(__dirname, "../", "data", "products.json");
+
+const getProductsFromFile = (callBack) => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      callBack([]);
+    } else {
+      callBack(JSON.parse(fileContent));
+    }
+  });
+};
 
 module.exports = class Product {
-    constructor(title) {
-        this.title = title;
-    }
+  constructor(title) {
+    this.title = title;
+  }
 
-    save() {
-        products.push(this.title);
-    }
+  save() {
+    getProductsFromFile((products) => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
+  }
 
-    static fetchAll() {
-        return products;
-    }
-}
+  static fetchAll(callBack) {
+    getProductsFromFile(callBack);
+  }
+};
