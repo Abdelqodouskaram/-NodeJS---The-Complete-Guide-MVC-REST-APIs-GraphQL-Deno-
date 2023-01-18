1ed34dad437805b773a11c5e75e4d920a6739658
AbdElQodous <a.karam@intcore.net>
2023-01-18 21:51:53 +0200

    first commit

--- app.js
+++ app.js
@@ -5,13 +5,15 @@ const bodyParser = require("body-parser");
 
 const errorController = require("./controllers/error");
 
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+const sequelize = require("./util/database");
+
 const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", "views");
-
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -21,4 +23,7 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(8080);
+sequelize
+  .sync()
+  .then(() => app.listen(8080))
+  .catch((err) => console.log(err));
